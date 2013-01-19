e5baab8575966972d23ab459ba9e3c8d03fd39ef
James Halliday <mail@substack.net>
2013-01-18 22:40:16 -0800

    portal.show() example with feedback reflection

--- example/world.js
+++ example/world.js
@@ -2,18 +2,38 @@ var createEngine = require('voxel-engine');
 
 var game = createEngine({
     generate: function(x, y, z) {
-        var height = 2;
         var d = Math.sqrt(x*x + y*y + z*z);
-        return d < 20 && y >= 0 && y <= 10;
+        return d < 20 && y >= -10 && (y < 0 || y*y <= 0.5*x*x + z*z / 64);
     },
     texturePath: './textures/',
-    materials: [ 'grass_top', 'tree_side', 'leaves_opaque' ]
+    materials: [ 'grass_top', 'obsidian', 'netherrack' ],
+    startingPosition: [ 0, 100, 0 ]
 });
 game.appendTo('#container');
+
+for (var z = -225; z <= 200; z+= 425) {
+    for (var y = 50; y <= 125; y += 25) {
+        for (var x = -75; x <= 50; x += 25) {
+            game.setBlock({ x: x, y: y, z: z }, 2);
+        }
+    }
+}
 
 var createPortal = require('../')(game);
 var explode = require('voxel-debris')(game);
 window.game = game;
+
+var a = createPortal({
+    x: 0, y: 100, z: 200,
+    width: 100, height: 50
+});
+var b = createPortal({
+    x: 0, y: 100, z: -200,
+    width: 100, height: 50
+});
+
+a.show(b, { x: 0, y: 0, z: 1 });
+b.show(a, { x: 0, y: 0, z: -1 });
 
 game.on('mousedown', function (pos) {
     if (erase) explode(pos)
--- index.js
+++ index.js
@@ -1,40 +1,62 @@
 var createCamera = require('voxel-camera');
+var EventEmitter = require('events').EventEmitter;
+var inherits = require('inherits');
 
 module.exports = function (game) {
-    var cameraControl = createCamera(game);
-    game.scene.add(cameraControl.camera());
+    return function (opts) {
+        return new Portal(game, opts || {});
+    };
+};
+
+function Portal (game, opts) {
+    var T = game.THREE;
+    this.game = game;
+    
+    var pos = new T.Vector3(opts.x || 0, opts.y || 0, opts.z || 0);
+    this.position = pos
+    
+    var width = opts.width === undefined ? 100 : opts.width;
+    this.width = width;
+    var height = opts.height === undefined ? 50 : opts.height;
+    this.height = height;
+    
+    var camera = this.camera = createCamera(game);
+    game.scene.add(camera.camera());
     
     var monitor = new game.THREE.Mesh(
-        new game.THREE.CubeGeometry(100, 100, 1),
+        new game.THREE.CubeGeometry(width, height, 0.5),
         new game.THREE.MeshBasicMaterial({
-            map: cameraControl.monitor()
+            map: camera.monitor()
         })
     );
-window.monitor = monitor;
-    
-    // Add monitor mesh to the scene
+    var p = monitor.position;
+    p.x = pos.x; p.y = pos.y; p.z = pos.z;
     game.scene.add(monitor);
+}
+
+inherits(Portal, EventEmitter);
+
+Portal.prototype.show = function (target, d) {
+    var self = this;
+    var T = self.game.THREE;
+    
+    var pos = target.position || target;
+    if (!d) d = new T.Vector3(0, 0, 1)
+    else d = new T.Vector3(d.x, d.y, d.z)
     
     var item = {
-        x: 0, y: 0, z: 0,
-        mesh: new game.THREE.Mesh(
-            new game.THREE.CubeGeometry(10, 10, 10),
-            new game.THREE.MeshBasicMaterial({
-                color: 0xff0000,
-                ambient: 0xff0000
-            })
+        mesh: new T.Mesh(
+            new T.CubeGeometry(10, 10, 10)
         )
     };
-item.mesh.position.y = 350;
-monitor.position.y = 350;
-    var offset = new game.THREE.Vector3(0, 350, 0);
-    var look = new game.THREE.Vector3(0, 0.2, 1);
-window.item = item;
-window.offset = offset;
-window.look = look;
+    var p = item.mesh.position;
+    p.x = pos.x; p.y = pos.y; p.z = pos.z;
     
-    // Then render the camera on tick
+    d.multiplyScalar(50);
+    var offset = new T.Vector3(pos.x, pos.y, pos.z).addSelf(d);
+    var look = new T.Vector3().add(offset, d);
+    
     game.on('tick', function(dt) {
-        cameraControl.render(item, offset, look);
+        self.camera.render(item, offset, look);
     });
 };
